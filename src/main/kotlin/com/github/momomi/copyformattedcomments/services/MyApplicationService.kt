8718482c0ace90803ca9caf14743f95fe1aee9e4
GitHub Action <action@github.com>
2022-07-17 10:31:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/momomi/copyformattedcomments/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.momomi.copyformattedcomments.services
+
+import com.github.momomi.copyformattedcomments.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
